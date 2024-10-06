ba960de2891cff4abe4af39b9592d0e197cdc62f
Roman Aebi <unity2.sae@gmail.com>
2024-10-06 12:53:33 +0200

    Importing Assets + XR Input as default

---
+++ Assets/Scripts/GameUI/SceneController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace GameUI {
+    public class SceneController : MonoBehaviour {
+        [SerializeField] private Button startButton;
+
+
+        private void OnEnable() {
+            startButton.onClick.AddListener(OnClickStartButton);
+        }
+        private void OnDisable() {
+            startButton.onClick.RemoveListener(OnClickStartButton);
+        }
+
+
+        private void OnClickStartButton() {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
