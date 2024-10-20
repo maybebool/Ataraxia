148d016b429e1e55bdcd4006de435090e2b4262e
Roman Aebi <unity2.sae@gmail.com>
2024-10-20 11:57:07 +0200

    Singelton Script and LoadingPlayer script

---
+++ Assets/Scripts/SceneHandling/LoadPlayerPersistent.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace SceneHandling {
+    public class LoadPlayerPersistent : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (!Application.isEditor)
+                LoadPersistent();
+        }
+
+        private void LoadPersistent()
+        {
+            SceneManager.LoadSceneAsync("Scenes/MainMenu", LoadSceneMode.Additive);
+        }
+    }
+}
