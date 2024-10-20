3edbb2a9289ca5e8a5384e72c05b6e5bc0c2e43d
Roman Aebi <unity2.sae@gmail.com>
2024-10-20 13:55:01 +0200

    ScreenFader.cs changes, Graphics.Blit not working in URP

--- Assets/Scripts/SceneHandling/ScreenFader.cs
+++ Assets/Scripts/SceneHandling/ScreenFader.cs
@@ -1,47 +1,124 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ScreenFader : MonoBehaviour
-{
-    [SerializeField] private float _speed = 1.0f;
-    [SerializeField] private float _intensity = 0.0f;
-    [SerializeField] private Color _color = Color.black;
-    [SerializeField] private Material _fadeMaterial = null;
-
-    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+namespace SceneHandling {
+    public class ScreenFader : MonoBehaviour
     {
-        _fadeMaterial.SetFloat("_Intensity", _intensity);
-        _fadeMaterial.SetColor("_FadeColor", _color);
-        Graphics.Blit(source, destination, _fadeMaterial);
-    }
+    //     [SerializeField] private float _speed = 1.0f;
+    //     [SerializeField] private float _intensity = 0.0f;
+    //     [SerializeField] private Color _color = Color.black;
+    //     [SerializeField] private Material _fadeMaterial = null;
+    //
+    //     private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    //     {
+    //         _fadeMaterial.SetFloat("_Intensity", _intensity);
+    //         _fadeMaterial.SetColor("_FadeColor", _color);
+    //         Graphics.Blit(source, destination, _fadeMaterial);
+    //     }
+    //
+    //     public Coroutine StartFadeIn()
+    //     {
+    //         StopAllCoroutines();
+    //         return StartCoroutine(FadeIn());
+    //     }
+    //
+    //     private IEnumerator FadeIn()
+    //     {
+    //         while (_intensity <= 1.0f)
+    //         {
+    //             _intensity += _speed * Time.deltaTime;
+    //             yield return null;
+    //         }
+    //     }
+    //
+    //     public Coroutine StartFadeOut()
+    //     {
+    //         StopAllCoroutines();
+    //         return StartCoroutine(FadeOut());
+    //     }
+    //
+    //     private IEnumerator FadeOut()
+    //     {
+    //         while (_intensity >= 0.0f)
+    //         {
+    //             _intensity -= _speed * Time.deltaTime;
+    //             yield return null;
+    //         }
+    //     }
+    // }
+    
+        [SerializeField] private float _speed = 1.0f;
+        [SerializeField, Range(0f, 1f)] private float _intensity = 0.0f;
+        [SerializeField] private Color _color = Color.black;
 
-    public Coroutine StartFadeIn()
-    {
-        StopAllCoroutines();
-        return StartCoroutine(FadeIn());
-    }
+        private Image _fadeImage;
 
-    private IEnumerator FadeIn()
-    {
-        while (_intensity <= 1.0f)
+        private void Awake()
         {
-            _intensity += _speed * Time.deltaTime;
-            yield return null;
+            // Create a full-screen UI Image
+            GameObject canvasObject = new GameObject("ScreenFaderCanvas");
+            canvasObject.transform.SetParent(transform, false);
+
+            Canvas canvas = canvasObject.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.sortingOrder = short.MaxValue; // Ensure it's on top
+
+            CanvasScaler canvasScaler = canvasObject.AddComponent<CanvasScaler>();
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+
+            // Create the Image
+            GameObject imageObject = new GameObject("FadeImage");
+            imageObject.transform.SetParent(canvasObject.transform, false);
+
+            _fadeImage = imageObject.AddComponent<Image>();
+            _fadeImage.color = new Color(_color.r, _color.g, _color.b, _intensity);
+            _fadeImage.rectTransform.anchorMin = Vector2.zero;
+            _fadeImage.rectTransform.anchorMax = Vector2.one;
+            _fadeImage.rectTransform.offsetMin = Vector2.zero;
+            _fadeImage.rectTransform.offsetMax = Vector2.zero;
         }
-    }
 
-    public Coroutine StartFadeOut()
-    {
-        StopAllCoroutines();
-        return StartCoroutine(FadeOut());
-    }
-
-    private IEnumerator FadeOut()
-    {
-        while (_intensity >= 0.0f)
+        public Coroutine StartFadeIn()
         {
-            _intensity -= _speed * Time.deltaTime;
-            yield return null;
+            StopAllCoroutines();
+            return StartCoroutine(FadeIn());
+        }
+
+        private IEnumerator FadeIn()
+        {
+            while (_intensity < 1.0f)
+            {
+                _intensity += _speed * Time.deltaTime;
+                _intensity = Mathf.Clamp01(_intensity);
+                UpdateFadeImage();
+                yield return null;
+            }
+        }
+
+        public Coroutine StartFadeOut()
+        {
+            StopAllCoroutines();
+            return StartCoroutine(FadeOut());
+        }
+
+        private IEnumerator FadeOut()
+        {
+            while (_intensity > 0.0f)
+            {
+                _intensity -= _speed * Time.deltaTime;
+                _intensity = Mathf.Clamp01(_intensity);
+                UpdateFadeImage();
+                yield return null;
+            }
+        }
+
+        private void UpdateFadeImage()
+        {
+            if (_fadeImage != null)
+            {
+                _fadeImage.color = new Color(_color.r, _color.g, _color.b, _intensity);
+            }
         }
     }
 }
