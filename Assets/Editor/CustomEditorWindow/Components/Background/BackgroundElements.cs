e90cfcbcb26dc75249db0184a23b8ced9504c600
Roman Aebi <unity2.sae@gmail.com>
2024-11-09 21:21:17 +0100

    Testing UXML Approach

---
+++ Assets/Editor/CustomEditorWindow/Components/Background/BackgroundElements.cs
@@ -0,0 +1,30 @@
+using UnityEditor.Experimental;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Editor.CustomEditorWindow.Components.Background {
+    [UxmlElement("Background")]
+    public partial class BackgroundElements : VisualElement {
+        private VisualElement backgroundImage { get; set; }
+
+        public BackgroundElements() {
+            var asset = Resources.Load<VisualTreeAsset>("BackgroundElements");
+            asset.CloneTree(this);
+            backgroundImage = this.Q("backgroundImage");
+        }
+
+        public void SetBackGroundIMage(Texture2D image) {
+            backgroundImage.style.backgroundImage = image;
+        }
+        
+        // public BackgroundElements(Texture2D image)
+        // {
+        // }
+        
+        // public void Init(Texture2D image)
+        // {
+        //     backgroundImage.style.backgroundImage = image;
+        // }
+       
+    }
+}
---
+++ Assets/Editor/CustomEditorWindow/Components/UpperMainButton/UpperMainButton.cs
@@ -0,0 +1,16 @@
+using UnityEngine.UIElements;
+
+namespace Editor.CustomEditorWindow.Components.UpperMainButton {
+    [UxmlElement("UpperMainButton")]
+    public partial class UpperMainButton : Button {
+        
+        // private Button upperMainButton => this.Q("upperMainButton");
+        public UpperMainButton() {
+            
+        }
+
+        public UpperMainButton(Button button) {
+            
+        }
+    }
+}
