7d33091963511604162f266e1706a1f2619e520a
Roman Aebi <unity2.sae@gmail.com>
2024-12-03 14:16:38 +0100

    cleaning scripts

--- Assets/Scripts/Managers/MtsEventManager.cs
+++ Assets/Scripts/Managers/MtsEventManager.cs
@@ -2,45 +2,35 @@
 
 namespace Managers {
     public class MtsEventManager : Singleton<MtsEventManager> {
-
-        // Events
+        
         public delegate void ButtonPressedAction();
         public event ButtonPressedAction OnButtonPressed;
-
         public delegate void ButtonReleasedAction();
         public event ButtonReleasedAction OnButtonReleased;
-
-        // Input action asset
+        
         private XRIDefaultInputActions _inputActions;
 
         private void Awake() {
             _inputActions = new XRIDefaultInputActions();
         }
 
-        private void OnEnable()
-        {
+        private void OnEnable() {
             _inputActions.XRIRightLocomotion.TeleportMode.Enable();
-
-            // Register callbacks for button press and release
             _inputActions.XRIRightLocomotion.TeleportMode.performed += HandleButtonPressed;
             _inputActions.XRIRightLocomotion.TeleportMode.canceled += HandleButtonReleased;
         }
 
-        private void OnDisable()
-        {
+        private void OnDisable() {
             _inputActions.XRIRightLocomotion.TeleportMode.performed -= HandleButtonPressed;
             _inputActions.XRIRightLocomotion.TeleportMode.performed -= HandleButtonReleased;
-
             _inputActions.XRIRightLocomotion.TeleportMode.Disable();
         }
 
-        private void HandleButtonPressed(InputAction.CallbackContext context)
-        {
+        private void HandleButtonPressed(InputAction.CallbackContext context) {
             OnButtonPressed?.Invoke();
         }
 
-        private void HandleButtonReleased(InputAction.CallbackContext context)
-        {
+        private void HandleButtonReleased(InputAction.CallbackContext context) {
             OnButtonReleased?.Invoke();
         }
     }
