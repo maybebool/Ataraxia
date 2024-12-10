d2513da22e81b340f556495bc646ce4199d460bd
Roman Aebi <unity2.sae@gmail.com>
2024-12-10 19:47:32 +0100

    cleaning script

--- Assets/Editor/Components/SettingsPage/AudioSettings.cs
+++ Assets/Editor/Components/SettingsPage/AudioSettings.cs
@@ -1,5 +1,4 @@
 using Audio;
-using Editor.Helpers;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,8 +7,6 @@ namespace Editor.Components.SettingsPage {
         private string volumeName = "Music";
         private float volume;
         private Slider slider;
-        
-        
         
         public AudioSettings() {
 
@@ -31,21 +28,15 @@ namespace Editor.Components.SettingsPage {
             
             var mySlider = this.Q<Slider>("MusicSlider");
             var volumeLabel = this.Q<Label>("MusicValue");
-            // slider = mySlider;
             mySlider.value = 100;
-            
-            // TODO this doesnt work. Somehow .Q doesnt get the name i guess
-
             mySlider.RegisterValueChangedCallback(evt => {
                 var newValue = evt.newValue;
                 OnSliderValueChanged(volumeLabel, newValue);
                     
             });
-
         }
+        
         private void OnSliderValueChanged(Label volumeLabel, float value) {
-            // Settings.profile.GetAudioLevels();
-            Debug.Log("Slider reached via name");
             if (volumeLabel != null) {
                 volumeLabel.text = Mathf.Round(value * 100f) + "%";
             }
@@ -54,15 +45,5 @@ namespace Editor.Components.SettingsPage {
                 Settings.profile.SetAudioLevels(volumeName, value);
             }
         }
-        
-        public void ResetSliderValue(Label volumeLabel) {
-            if (Settings.profile) {
-                var volume = 1;
-                
-                OnSliderValueChanged(volumeLabel, volume);
-                slider.value = volume;
-            }
-        }
-    
     }
 }
